c02b7ac601926477d1051e23100c37b97c1340f6
o.wouters <o.wouters@ringring.be>
2017-10-21 14:00:58 +0200

    Test project (.net standard)

---
+++ src/Test/SmootherFixture.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace ServiceStack.Smoothie.Test
+{
+    public class SmootherFixture
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ src/Test/TimerFixture.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ServiceStack.Smoothie.Test
+{
+    public class TimerFixture
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
