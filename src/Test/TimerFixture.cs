b7177bf872750a02f2c5274ba3341326534802eb
o.wouters <o.wouters@ringring.be>
2017-10-21 14:33:56 +0200

    add servicestack and in memory sqlite to test project

--- src/Test/TimerFixture.cs
+++ src/Test/TimerFixture.cs
@@ -1,14 +1,70 @@
 using System;
+using ServiceStack.Data;
+using ServiceStack.OrmLite;
+using ServiceStack.Testing;
 using Xunit;
 
 namespace ServiceStack.Smoothie.Test
 {
-    public class TimerFixture
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class TimerService : Service
     {
+        public Timer Post(Timer request)
+        {
+            if(request.Id == Guid.Empty)
+                request.Id = Guid.NewGuid();
+
+            Db.Save(request);
+            
+            return request;
+        }
+    }
+
+    public class Timer
+    {
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global
+        public DateTime Time { get; set; }
+        public Guid Id { get; set; }
+    }
+
+    public class TimerExpired
+    {
+        public Guid Id { get; set; }
+    }
+    
+    public class TimerFixture : IDisposable
+    {
+        private readonly TimerService _svc;
+
+        public TimerFixture()
+        {
+            var appHost = new BasicAppHost().Init();
+            var container =  appHost.Container;
+
+            container.Register<IDbConnectionFactory>(
+                new OrmLiteConnectionFactory(":memory:", SqliteDialect.Provider));
+
+            container.RegisterAutoWired<TimerService>();
+
+            using (var db = container.Resolve<IDbConnectionFactory>().Open())
+            {
+                db.DropAndCreateTable<Timer>();
+            }
+
+            _svc = container.Resolve<TimerService>();
+        }
+        
         [Fact]
         public void Test()
         {
+            _svc.Post(new Timer{Time = DateTime.Now.AddHours(1)});
+            
             Assert.True(true);
+        }
+
+        public void Dispose()
+        {
+            _svc?.Dispose();
         }
     }
 }
